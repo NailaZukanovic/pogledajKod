89f01b44117b039bbbb002952c183daa7ec1add3
Naila Zukanovic <86067688+NailaZukanovic@users.noreply.github.com>
2021-11-15 14:56:05 +0100

    Update user.js

--- api/models/user.js
+++ api/models/user.js
@@ -2,6 +2,9 @@ const mongoose = require('mongoose');
 
 const userSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
+    name: { type: String, required: true }, 
+    lastName: { type: String, required: true },
+    sex:  { type: String, required: true },
     email: { 
         type: String, 
         required: true, 
