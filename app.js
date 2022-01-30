5849a5e57940fcdb4182f34569497b939a6610c2
Naila <nailazukanovic@gmail.com>
2022-01-30 21:20:55 +0100

    aaa

--- app.js
+++ app.js
@@ -88,4 +88,13 @@ app.use( (error,req,res, next) =>
     });
 });
 
+if(process.env.NODE_ENV === 'production'){    
+    app.use(express.static('frontend/build'))  // set static folder 
+    //returning frontend for any route other than api 
+    app.get('*',(req,res)=>{     
+        res.sendFile (path.resolve(__dirname,'frontend','build',         
+                      'index.html' ));    
+    });
+}
+
 module.exports = app;
