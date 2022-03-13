f86e2933ec3ac270ea627ada1f35b794ea0caa77
Naila Zukanovic <86067688+NailaZukanovic@users.noreply.github.com>
2022-03-13 10:14:42 +0100

    Delete frontend/src directory

--- frontend/src/ApiClient.jsx
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-import config from "./config";
-
-const instance = axios.create({
-    baseURL: 'http://localhost:4000/user',
-});
-
-export default instance;
--- frontend/src/Components/Game.test.jsx
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import Game from './Game';
-
-test('renders 12 game cards', () => {
-  render(<Game />);
-  const gameCards = screen.getAllByRole('img', { name: /card front/i });
-  expect(gameCards).toHaveLength(12);
-});
